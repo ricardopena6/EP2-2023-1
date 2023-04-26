696dd348a844aeda42064268981361a74dbe2f21
Ricardo <ricardop6%al.insper.edu.brl>
2023-04-26 17:22:28 -0300

    Função 1 Feita

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print('hello world')
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    for i in range(tamanho):
+        if orientacao == 'horizontal':
+            posicoes.append([linha, coluna + i])
+        else:
+            posicoes.append([linha + i, coluna])
+    return posicoes
